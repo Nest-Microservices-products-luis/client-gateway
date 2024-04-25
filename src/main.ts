54322a3ca2cbd6c14c7eeb0512463c584364130e
Luis <luisvaleroc@gmail.com>
2024-04-25 15:50:52 -0400

    add coonsole log

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
    app.useGlobalFilters(new RpcCustomExceptionFilter());
   await app.listen(envs.port);
 
+  console.log("hola mundo primer cambio")
   logger.log(`Gateway running on port ${envs.port}`);
 }
 bootstrap();
